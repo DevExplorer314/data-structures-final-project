374a95b315fe31dc64dedc00e562a2f3cd57eb4c
sfss2509 <83672889+sfss2509@users.noreply.github.com>
2021-05-12 21:39:39 +0100

    Update projeto_final.py

--- projeto_final.py
+++ projeto_final.py
@@ -173,6 +173,9 @@ class Graph:
             del self._incoming[v]
         return v
 
+    def get_weight(self):
+        return
+
 """2. Método de carregamento de dados de um ficheiro csv que obedeça ao seguinte formato:
     i) por linha existem 3 valores de dados - o 1.º e o 2.º indicam nomes de vértices e o 3.º um peso. 
     ii) A 1.ª linha do ficheiro indica o nome das colunas.
@@ -242,11 +245,52 @@ def shortest_path(graph, start, goal):
 
 
 """ (b) usando os pesos nas arestas"""
-def shortest_path_weight():
-    '''TODO: Implementar o método utilizado no link abaixo.
-    Link: https://www.youtube.com/watch?v=Ub4-nG09PFw
-    '''
-    pass
+graph = {'a': {'b': 10, 'c': 3}, 'b': {'c': 1, 'd': 2}, 'c': {'b': 4, 'd': 8, 'e': 2}, 'd': {'e': 7}, 'e': {'d': 9}}
+
+def dijkstra(graph, start, goal):
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = graph
+    infinity = 9999999
+    path = []
+    for node in unseenNodes:
+        shortest_distance[node] = infinity
+    shortest_distance[start] = 0
+
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+
+        for childNode, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
+        unseenNodes.pop(minNode)
+
+    currentNode = goal
+    while currentNode != start:
+        try:
+            path.insert(0, currentNode)
+            currentNode = predecessor[currentNode]
+        except KeyError:
+            print('Path not reachable')
+            break
+    path.insert(0, start)
+    if shortest_distance[goal] != infinity:
+        print('Shortest distance is ' + str(shortest_distance[goal]))
+        print('And the path is ' + str(path))
+
+dijkstra(graph, 'a', 'b')
+
+
+
+
+
+
 
 if __name__ == "__main__":
 
